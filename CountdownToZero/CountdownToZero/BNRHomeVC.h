218f79ec48df72615be2fc74ed042d063ff7751b
Christian Keur <cbkeur@gmail.com>
2013-05-01 19:39:38 -0400

    Initial VC structure

---
+++ CountdownToZero/CountdownToZero/BNRHomeVC.h
@@ -0,0 +1,13 @@
+//
+//  BNRHomeVC.h
+//  CountdownToZero
+//
+//  Created by Christian Keur on 5/1/13.
+//  Copyright (c) 2013 Christian Keur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHomeVC : UIViewController
+
+@end
